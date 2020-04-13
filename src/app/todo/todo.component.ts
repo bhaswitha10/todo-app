2c35c93a7dd2ebec510e29adc9355a73f8dac06f
Raviteja <teja.22060@gmail.com>
2020-04-13 14:29:40 +0530

    Made changes for package lock joson

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from './shared/todo.service';
-import { element } from 'protractor';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
