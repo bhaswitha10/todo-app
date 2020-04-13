1e118e7e84205e15e8f5c07f6cbdf3274d57cfbc
bhaswitha10 <sribhaswitha@gmail.com>
2020-04-13 14:16:12 +0530

    initial checkin

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAvRscDnPISzLZvHJRglFsOZXNIcIilUII",
+    authDomain: "to-do-app-e94f6.firebaseapp.com",
+    databaseURL: "https://to-do-app-e94f6.firebaseio.com",
+    projectId: "to-do-app-e94f6",
+    storageBucket: "to-do-app-e94f6.appspot.com",
+    messagingSenderId: "408792203687",
+    appId: "1:408792203687:web:aafdaaaa645a05afe97682",
+    measurementId: "G-3ELYS56ZBH"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
